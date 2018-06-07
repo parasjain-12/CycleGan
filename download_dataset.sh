23ce1ad7c8fc8527c928f6b4a649b50a171a003f
Paras Jain <jainparas249@gmail.com>
2018-06-07 17:33:53 +0530

    Add files via upload

---
+++ download_dataset.sh
@@ -0,0 +1,15 @@
+mkdir datasets
+FILE=$1
+
+if [[ $FILE != "cityscapes" ]]; then
+    echo "Available datasets are: apple2orange, summer2winter_yosemite, horse2zebra, monet2photo, cezanne2photo, ukiyoe2photo, vangogh2photo, maps, cityscapes, facades, iphone2dslr_flower, ae_photos"
+    exit 1
+fi
+
+URL=https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/$FILE.zip
+ZIP_FILE=./datasets/$FILE.zip
+TARGET_DIR=./datasets/$FILE/
+wget -N $URL -O $ZIP_FILE
+mkdir $TARGET_DIR
+unzip $ZIP_FILE -d ./datasets/
+rm $ZIP_FILE
